dd7cf95f6ec88ef55a3e3671e716f9248763b4d9
Kræn Hansen (Socialsquare) <kraen@socialsquare.dk>
2017-02-22 22:27:44 +0100

    Restyled the chaos exceptions

--- templates/chaos-exception.php
+++ templates/chaos-exception.php
@@ -1,12 +1,12 @@
-<h1 class="text-error">Woups ... Something unexpected happend in the CHAOS:\_</h1>
-<?php if(WP_DEBUG):?>
-	<h2 class="text-error">As WordPress is running in debug mode - this is probably your fault.</h2>
-<?php else:?>
-	<h2 class="text-error">Don't you worrie - this is probably not your fault.</h2>
-<?php endif;?>
-<?php if($exception):?>
-	<p class="text-error"><?php echo htmlentities($exception->getMessage()) ?></p>
-	<pre class="text-error"><?php echo htmlentities($exception->getTraceAsString()) ?></pre>
-<?php else:?>
-	<p class="text-error">The exception stacktrace was removed.</p>
-<?php endif;?>
+<div style="font-size:12px;border:1px solid gray;padding:10px;overflow-x:auto;color:#eee;background:rgba(0,0,0,.75);">
+	<h1 style="font-size:16px;color:#eee;">Woups ... Something unexpected happend in the CHAOS:\_</h1>
+	<?php if(!WP_DEBUG):?>
+		<p>Don't you worrie - this is probably not your fault.</p>
+	<?php endif;?>
+	<?php if($exception):?>
+		<p><?php echo htmlentities($exception->getMessage()) ?></p>
+		<pre><?php echo htmlentities($exception->getTraceAsString()) ?></pre>
+	<?php else:?>
+		<em>The exception stacktrace was removed, see <?= $traceDumpFile ?> for details.</em>
+	<?php endif;?>
+</div>
